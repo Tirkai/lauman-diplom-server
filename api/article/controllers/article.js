cf2daf83118f3a4d9d833f16742838101cec7663
Дмитрий Буренков <burenkov@algont.ru>
2020-05-31 20:05:24 +0300

    Add score

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -4,57 +4,21 @@
  * to customize this controller
  */
 
-const calculateEntitiesScore = (entities) => {
-    let result = [];
-    entities.forEach((item) => {
-        item["score"] =
-            item.score
-                .map((score) => score.value)
-                .reduce((accumulator, current) => accumulator + current) /
-            item.score.length;
-        result.push(item);
-    });
-    return result;
-};
+const vote = async (ctx) => {
+    const id = parseInt(ctx.request.query.id);
+    let [article] = await strapi.services.article.find({ id });
+    const score = parseInt(ctx.request.query.score);
 
-const find = async (ctx) => {
-    let entities = await strapi.services.article.find(ctx.query);
-
-    return calculateEntitiesScore(entities);
-};
-
-const setScore = async (ctx) => {
-    const id = parseInt(ctx.request.body.id);
-    const [article] = await strapi.services.article.find({ id });
-    const score = ctx.request.body.score;
-
-    if (+score && score > 0 && score <= 5) {
-        article.score.push({
-            value: ctx.request.body.score,
-        });
-        await strapi.services.article.update({ id }, article);
-        return article;
+    if (score < 0) {
+        article.score.downvote += -score;
     } else {
-        return {
-            statusCode: 500,
-            error: "Out of range",
-            message: "Out of range",
-        };
+        article.score.upvote += score;
     }
-};
 
-const getTopScored = async (ctx) => {
-    let entities = await strapi.services.article.find(ctx.query);
-
-    const sortedByScore = calculateEntitiesScore(entities)
-        .sort((first, second) => second.score - first.score)
-        .slice(0, 10);
-
-    return sortedByScore;
+    await strapi.services.article.update({ id }, article);
+    return article;
 };
 
 module.exports = {
-    find,
-    setScore,
-    getTopScored,
+    vote,
 };
